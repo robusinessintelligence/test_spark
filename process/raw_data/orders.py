c28ed2e4f741bb189a685eabf01b704d4603ce27
Raphael Oliveira <raphaelbrf@gmail.com>
2026-01-26 22:53:52 -0300

    developing orders

--- process/raw_data/orders.py
+++ process/raw_data/orders.py
@@ -37,116 +37,81 @@ df_orders = (
         .load("/jobs/input/orders.json")
 )
 
-df_orders.show()
+df_orders.printSchema()
 
-# handle status values
-df_orders_transform_data = df_orders.withColumn(
-    "status",
-    f.upper(f.col("status"))
+
+# handle with null fields
+check_null_columns = df_orders.columns
+find_null_values = " OR ".join([f'{col} IS NULL' for col in check_null_columns])
+df_null_fields = df_orders.filter(find_null_values)
+
+
+# clean data
+df_orders_cleaned = (
+    df_orders
+        .join(
+            df_null_fields,
+            on="order_id",
+            how="left_anti"
+        )
 )
 
 
+# handle with data transformations
+df_orders_transform_data = df_orders_cleaned.select(
+    f.col("order_id"),
+    f.abs(f.col("amount")).alias("amount"),
+    f.col("currency"),
+    f.col("customer_id"),
+
+    f.coalesce(
+        f.try_to_date(f.col("order_date"), "yyyy-MM-dd"),
+        f.try_to_date(f.col("order_date"), "dd-MM-yyyy"),
+        f.try_to_date(f.col("order_date"), "dd/MM/yyyy"),
+    ).alias("order_date"),
+
+    f.upper(f.col("status")).alias("status"),
+)
+
 df_orders_transform_data.show()
-
-
-check_null_columns = df_orders_transform_data.columns
-find_null_values = " OR ".join([f'{col} IS NULL' for col in check_null_columns])
-df_null_fields = df_orders_transform_data.filter(find_null_values)
-
-df_null_fields.show()
         
 
-
-
-# # handle with duplicated ids
-# deduplicate_window = Window.partitionBy("customer_id").orderBy(f.col("created_at").desc())
-
-# df_customers_deduplicated = df_customers.withColumn(
-#     "duplicate_regs",
-#     f.row_number().over(deduplicate_window)
-# )
-
-# # get duplicated ids
-# df_duplicated_ids = df_customers_deduplicated.where("duplicate_regs > 1")
-
-
-# # handle with invalid regs
-# check_null_columns = df_customers_deduplicated.columns
-# find_null_values = " OR ".join([f'{col} IS NULL' for col in check_null_columns])
-
-# # get null values
-# df_invalid_regs = df_customers_deduplicated.filter(find_null_values)
-
-
-# # cleaned data
-# df_customers_cleaned = (
-#     df_customers_deduplicated
-#         .where("duplicate_regs = 1")
-#         .join(
-#             df_invalid_regs,
-#             on="customer_id",
-#             how="left_anti"
-#         )
-#         .drop("duplicate_regs")
-# )
-
-# # df_customers_cleaned = (
-# #     df_customers_deduplicated
-# #         .where("duplicate_regs = 1")
-# #         .na.drop(subset=["name", "country"])
-# #         .drop("duplicate_regs")
-# # )
-
 # ################################################################################
-# # save duplicated
-# df_duplicated_ids = (
-#     df_duplicated_ids
-#         .withColumn("processing_date", f.current_date())
-#         .withColumn("error_timestamp", f.current_timestamp())
-#         .withColumn("error_reason", f.lit("Duplicated regs"))
+# save null fields
+df_null_fields = (
+    df_null_fields
+        .withColumn("processing_date", f.current_date())
+        .withColumn("error_timestamp", f.current_timestamp())
+        .withColumn("error_reason", f.lit("null fields regs"))
 
-# )
+)
 
-# (
-#     df_duplicated_ids.write
-#         .mode("overwrite")
-#         .partitionBy("processing_date")
-#         .format("csv")
-#         .save("/jobs/rejected_data/customers_duplicated_ids")
-# )
+(
+    df_null_fields.write
+        .mode("overwrite")
+        .partitionBy("processing_date")
+        .format("csv")
+        .save("/jobs/rejected_data/orders/null_fields")
+)
 
-# ################################################################################
-# # save invalid regs
-# df_invalid_regs = (
-#     df_invalid_regs
-#         .withColumn("processing_date", f.current_date())
-#         .withColumn("error_timestamp", f.current_timestamp())
-#         .withColumn("error_reason", f.lit("Missing data fields"))
-# )
+################################################################################
+# save clean data
+df_orders_transform_data = (
+    df_orders_transform_data
+        .withColumn("processing_date", f.current_date())
+)
 
-# (
-#     df_invalid_regs.write
-#         .mode("overwrite")
-#         .partitionBy("processing_date")
-#         .format("csv")
-#         .save("/jobs/rejected_data/customers_null_fields")
-# )
+(
+    df_orders_transform_data.write
+        .mode("overwrite")
+        .partitionBy("processing_date")
+        .format("parquet")
+        .save("/jobs/raw_data/orders")
+)
 
-# ################################################################################
-# # save clean data
-# df_customers_cleaned = (
-#     df_customers_cleaned
-#         .withColumn("processing_date", f.current_date())
-# )
+df_null_fields.show()
+df_orders_transform_data.show()
 
-# (
-#     df_customers_cleaned.write
-#         .mode("overwrite")
-#         .partitionBy("processing_date")
-#         .format("parquet")
-#         .save("/jobs/raw_data/customers")
-# )
+logger.info(f"total time process: {datetime.now() - start_time}")
 
-# logger.info(f"total time process: {datetime.now() - start_time}")
-
-# spark.stop()
+spark.stop()
